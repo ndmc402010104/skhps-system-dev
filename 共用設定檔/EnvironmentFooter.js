bcb45cc37afa637df16da0e0342001ab8a2619ee
益昇 石 <ndmc402010104@gmail.com>
2026-06-05 12:08:49 +0800

    換電腦測試前部署正式版

--- 共用設定檔/EnvironmentFooter.js
+++ 共用設定檔/EnvironmentFooter.js
@@ -40,7 +40,7 @@ EnvironmentFooter.js
         key:'webProd',
         label:'正式版',
         url:'https://skhps.jonaminz.com',
-        version:'v2.30.0-202606050926',
+        version:'v2.31.0-202606051207',
         type:'web'
       }
     };
@@ -255,6 +255,7 @@ EnvironmentFooter.js
     renderEnvironmentFooter();
   }
 })(typeof window !== 'undefined' ? window : this);
+
 
 
 
